053e7bb72b58b538a729121b7d621bea351258a6
Fazzani <tunisienheni@outlook.com>
2015-08-31 14:33:16 +0200

    HFA: Fix prod bower

--- src/GruntTest/gruntfile.js
+++ src/GruntTest/gruntfile.js
@@ -1,7 +1,7 @@
 /// <binding AfterBuild='dist' ProjectOpened='watch:tasks, jsonServer, webServer' />
 module.exports = function (grunt) {
     grunt.initConfig({
-        clean: ["wwwroot/dist/*", "wwwroot/temp/*"],
+        clean: ["wwwroot/dist/*", "wwwroot/temp/*", "dist/js/*","dist/styles/*"],
         bower: {
             install: {
                 options: {
@@ -9,6 +9,20 @@ module.exports = function (grunt) {
                     layout: "byComponent",
                     cleanTargetDir: false
                 }
+            }
+        },
+        cssmin: {
+            dev: {
+                expand: true,
+                cwd: 'wwwroot/Content/Styles',
+                src: ['*.css'],
+                dest: 'wwwroot/dist/styles'
+            },
+            publish: {
+                expand: true,
+                cwd: 'wwwroot/Content/Styles',
+                src: ['*.css'],
+                dest: 'dist/styles'
             }
         },
         jshint: {
@@ -27,6 +41,10 @@ module.exports = function (grunt) {
             all: {
                 src: ['wwwroot/temp/blsComponents.js'],
                 dest: 'wwwroot/dist/blsComponents.min.js'
+            },
+            publish: {
+                src: ['wwwroot/temp/blsComponents.js'],
+                dest: 'dist/js/blsComponents.min.js'
             }
         },
         watch: {
@@ -39,6 +57,45 @@ module.exports = function (grunt) {
             },
             json_server: {
                 command: 'json-server --watch dbPersons.json'
+            },
+            bumpVersion: {
+                command: 'npm version patch'
+            }
+        },
+        aws: grunt.file.readJSON('grunt-aws.json'),
+        s3: {
+            options: {
+                debug:false,
+                key: '<%= aws.key %>',
+                secret: '<%= aws.secret %>',
+                access: 'public-read',
+                headers: {
+                    // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
+                    "Cache-Control": "max-age=630720000, public",
+                    "Expires": new Date(Date.now() + 63072000000).toUTCString()
+                }
+            },
+            dev: {
+                options: {
+                    bucket: '<%= aws.buckets.dev %>'
+                },
+                upload: [
+                  {
+                      src: 'wwwroot/dist/*.*',
+                      dest: '/'
+                  }
+                ]
+            },
+            prod: {
+                options: {
+                    bucket: '<%= aws.buckets.prod %>'
+                },
+                upload: [
+                  {
+                      src: 'dist/**/*.*',
+                      dest: '/'
+                  }
+                ]
             }
         }
     });
@@ -48,6 +105,11 @@ module.exports = function (grunt) {
     grunt.registerTask("dist", ['clean', 'concat', 'jshint', 'uglify']);
     grunt.registerTask('jsonServer', ['shell:json_server']);
     grunt.registerTask('webServer', ['shell:express_server']);
+    //grunt.registerTask('minCss', ['cssmin:dev']);
+    grunt.registerTask('deployDev', ['s3:dev']);
+    grunt.registerTask('deployProd', ['s3:prod']);
+    grunt.registerTask("publishBower", ['clean', 'concat', 'jshint', 'cssmin:publish', 'uglify:publish']);
+
     // The following line loads the grunt plugins.
     // This line needs to be at the end of this this file.
     grunt.loadNpmTasks('grunt-shell');
@@ -55,7 +117,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     //grunt.loadNpmTasks("grunt-bower-task");
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-s3');
    
 };
