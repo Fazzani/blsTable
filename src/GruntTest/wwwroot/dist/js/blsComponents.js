2de170849bfd9536f2724d46a5f40e94b5868bbd
Fazzani <tunisienheni@outlook.com>
2015-09-25 17:56:24 +0200

    HFA: Fix one row display

--- src/GruntTest/wwwroot/dist/js/blsComponents.js
+++ src/GruntTest/wwwroot/dist/js/blsComponents.js
@@ -1292,13 +1292,13 @@ angular.module("bls_components").directive('blsRows', ['$log', '$compile', '$tem
     var link = function (scope, element, attrs, ctrls) {
         // var blsTableCtrl = ctrls[0];
         scope.getTdTpl = function (col, d) {
-
             if (col.tpl && col.tpl !== '') {
-                col.tpl = col.tpl.replace('::row', 'd');
-                //$log.debug('            col.tpl => ', col.tpl);
+
+                col.tpl = col.tpl.replace(new RegExp('::row', 'g'), 'd');
                 return col.tpl;
             }
         };
+
         var eleTpl = angular.element($templateCache.get('templates/blsRows.html'));
         if (scope.isHierarchical())
             eleTpl.attr('bls-row-child', '');
